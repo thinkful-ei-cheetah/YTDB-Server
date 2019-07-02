481732fa85c628d579be5b601d71a9a57060a3c4
rahmor <rahmor@yahoo.com>
2019-07-02 10:41:14 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+
+const app = express();
+
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+const whitelist = ['http://localhost:3000', 'http://my-project.com'];
+const options = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(morgan(morganOption));
+app.use(cors(options));
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
