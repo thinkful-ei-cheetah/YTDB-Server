59f77d7d003baad4f35bbff780b3ad4023fabbe7
rahmor <rahmor@yahoo.com>
2019-07-08 18:46:41 -0400

    change api lib to fetch

--- src/channels/channelApiService.js
+++ src/channels/channelApiService.js
@@ -1,52 +1,26 @@
 'use strict';
-const axios = require('axios');
+const fetch = require('node-fetch');
 const { YT_API_KEY } = require('../config');
 
 const baseURL = 'https://www.googleapis.com/youtube/v3';
 
 const ChannelApiService = {
   SearchChannels(searchterm) {
-    return axios
-      .get(`${baseURL}/search?`, {
-        params: {
-          part: 'snippet',
-          maxResults: '20',
-          order: 'relevance',
-          q: searchterm,
-          type: 'channel',
-          key: YT_API_KEY
-        },
-        headers: {
-          Accept: 'application/json'
-        }
-      })
-      .then(res => res.data);
+    return fetch(
+      `${baseURL}/search?part=snippet&maxResults=20&order=relevance&q=${searchterm}&type=channel&key=${YT_API_KEY}`
+    ).then(res => res.json());
   },
+
   SearchChannelsByTopic(searchterm, topic) {
-    return axios
-      .get(`${baseURL}/search?`, {
-        params: {
-          part: 'snippet',
-          maxResults: '20',
-          order: 'relevance',
-          q: searchterm,
-          topId: topic,
-          type: 'channel',
-          key: YT_API_KEY
-        },
-        headers: {
-          Accept: 'application/json'
-        }
-      })
-      .then(res => res.data);
+    return fetch(
+      `${baseURL}/search?part=snippet&maxResults=20&order=relevance&q=${searchterm}&topicId=${topic}&type=channel&key=${YT_API_KEY}`
+    ).then(res => res.json());
   },
 
   ChannelDetails(channelId) {
-    return axios
-      .get(
-        `${baseURL}/channels?part=snippet%2Cstatistics%2CtopicDetails%2CbrandingSettings&id=${channelId}&maxResults=1&key=${YT_API_KEY}`
-      )
-      .then(res => res.data);
+    return fetch(
+      `${baseURL}/channels?part=snippet%2Cstatistics%2CtopicDetails%2CbrandingSettings&id=${channelId}&maxResults=1&key=${YT_API_KEY}`
+    ).then(res => res.json());
   }
 };
 
