39d06e9d54af7796534aee0dd04dfc2a0eb822a1
rahmor <rahmor@yahoo.com>
2019-07-11 15:23:59 -0400

    add GET route to favorites router

--- src/favorite/favoriteService.js
+++ src/favorite/favoriteService.js
@@ -5,6 +5,14 @@ const FavoriteService = {
       .insert({ user_id, channel_id })
       .into('favorite')
       .returning('*');
+  },
+
+  getFavorites(knex, user_id) {
+    return knex('favorite')
+      .where({ user_id })
+      .join('channel', 'favorite.channel_id', '=', 'channel.id')
+      .select('channel.id', 'channel.title', 'channel.thumbnail')
+      .then(favorites => favorites);
   }
 };
 
