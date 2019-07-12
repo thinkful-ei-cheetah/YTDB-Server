1b978da9db6e94a15c2e0cea0b282d9d70770990
rahmor <rahmor@yahoo.com>
2019-07-12 13:55:06 -0400

    add update to channel table in channel rating router

--- src/channelrating/channelRatingService.js
+++ src/channelrating/channelRatingService.js
@@ -5,8 +5,20 @@ const ChannelRatingService = {
     return knex
       .insert({ user_id: id, channel_id: channelId, rating })
       .into('channel_rating')
-      .returning('*');
+      .returning('*')
+      .then(response => {
+        console.log(response);
+        return knex('channel')
+          .where('id', '=', response[0].channel_id)
+          .increment({
+            rating_count: 1,
+            rating_total: response[0].rating
+          })
+          .returning('*');
+      });
   }
 };
+
+//do plus equals to total and incrmeent count
 
 module.exports = ChannelRatingService;
