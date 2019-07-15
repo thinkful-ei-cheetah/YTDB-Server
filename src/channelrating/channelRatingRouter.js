2da0db340a4660bc2281146f802d42c472ca5e23
rahmor <rahmor@yahoo.com>
2019-07-15 12:59:08 -0400

    add reviews put to reviews router

--- src/channelrating/channelRatingRouter.js
+++ src/channelrating/channelRatingRouter.js
@@ -5,26 +5,30 @@ const jsonBodyParser = express.json();
 const { requireAuth } = require('../middleware/jwt-auth');
 const ChannelRatingService = require('./channelRatingService');
 
-ChannelRatingRouter.post('/', requireAuth, jsonBodyParser, (req, res, next) => {
-  const { rating, channelId } = req.body;
+ChannelRatingRouter.route('/')
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const { rating, channelId } = req.body;
 
-  for (const field of ['rating', 'channelId']) {
-    if (!req.body[field])
-      return res
-        .status(400)
-        .json({ error: `Missing ${field} in request body` });
-  }
+    for (const field of ['rating', 'channelId']) {
+      if (!req.body[field])
+        return res
+          .status(400)
+          .json({ error: `Missing ${field} in request body` });
+    }
 
-  ChannelRatingService.addUserRating(
-    req.app.get('db'),
-    req.user.id,
-    channelId,
-    rating
-  )
-    .then(response => {
-      res.status(201).json({ rating: response[0].rating });
-    })
-    .catch(error => next(error));
-});
+    ChannelRatingService.addUserRating(
+      req.app.get('db'),
+      req.user.id,
+      channelId,
+      rating
+    )
+      .then(response => {
+        res.status(201).json({ rating: response[0].rating });
+      })
+      .catch(error => next(error));
+  })
+  .get(jsonBodyParser, (req, res, next) => {
+    res.json({ hello: 'this workds' });
+  });
 
 module.exports = ChannelRatingRouter;
