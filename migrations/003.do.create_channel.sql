62bca25063ceca51f0524cd58d2f162a847ca7d3
levipaulk <levipaulk@gmail.com>
2019-07-16 17:44:36 -0400

    pre merge

--- migrations/003.do.create_channel.sql
+++ migrations/003.do.create_channel.sql
@@ -3,6 +3,6 @@ CREATE TABLE "channel" (
   yt_id TEXT NOT NULL UNIQUE,
   title VARCHAR NOT NULL,
   thumbnail TEXT,
-  rating_total INTEGER DEFAULT NULL,
-  rating_count INTEGER DEFAULT NULL
+  rating_total INTEGER DEFAULT 0,
+  rating_count INTEGER DEFAULT 0
 );
